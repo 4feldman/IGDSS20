56d93aa3b4a87773c3092f44d44c5dce51db8f5c
Johannes Coym <jomax@outlook.com>
2020-05-04 20:11:24 +0200

    Added Map Generation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Texture2D heightmap;
+
+    public GameObject waterPrefab;
+    public GameObject sandPrefab;
+    public GameObject grassPrefab;
+    public GameObject forestPrefab;
+    public GameObject stonePrefab;
+    public GameObject mountainPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < heightmap.height; i++)
+        {
+            for (int j = 0; j < heightmap.width; j++)
+            {
+                float val = heightmap.GetPixel(i,j).b;
+                Vector3 pos;
+                if (i % 2 == 1)
+                {
+                    pos = new Vector3(i * 8.66f, val * 10, j * 10 + 5);
+                }
+                else
+                {
+                    pos = new Vector3(i * 8.66f, val * 10, j * 10);
+                }
+
+
+                if(val == 0.0f)
+                {
+                    Instantiate(waterPrefab, pos, Quaternion.identity);
+                }
+                else if(val < 0.2f)
+                {
+                     Instantiate(sandPrefab, pos, Quaternion.identity);
+                }
+                else if(val < 0.4f)
+                {
+                     Instantiate(grassPrefab, pos, Quaternion.identity);
+                }
+                else if(val < 0.6f)
+                {
+                     Instantiate(forestPrefab, pos, Quaternion.identity);
+                }
+                else if(val < 0.8f)
+                {
+                     Instantiate(stonePrefab, pos, Quaternion.identity);
+                }
+                else
+                {
+                     Instantiate(mountainPrefab, pos, Quaternion.identity);
+                }
+            }
+        }
+    
     }
 
     // Update is called once per frame
