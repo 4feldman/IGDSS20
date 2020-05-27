07a664ab00d42da2195870f2557496533d3fe591
Johannes Coym <jomax@outlook.com>
2020-05-27 13:30:12 +0200

    Reworked Map Generation, added Building Contents

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public BuildingType type;
+    public int upkeep;
+    public int cost_money;
+    public int cost_planks;
+    public Tile tile;
+    public float efficiency = 1.0f;
+    public int resourceGenInterval;
+    public int resourceGenAmount;
+    public List<Tile.TileTypes> possibleTileTypes;
+    public Tile.TileTypes scalesWithNeighboringTiles = Tile.TileTypes.Empty;
+    public int minNeighbors;
+    public int maxNeighbors;
+    public List<GameManager.ResourceTypes> inputResources = new List<GameManager.ResourceTypes>();
+    public GameManager.ResourceTypes outputResource;
 
-    // Update is called once per frame
-    void Update()
+    public enum BuildingType {Null, Fishery, Lumberjack, Sawmill, SheepFarm, FrameworkKnitters, PotatoFarm, SchnappsDistillery};
+
+    public void generateEfficiency(List<Tile> neighborTiles)
     {
-        
+        if(scalesWithNeighboringTiles != Tile.TileTypes.Empty)
+        {
+            int fittingNeighbors = 0;
+            foreach(Tile neighbor in neighborTiles)
+            {
+                if (neighbor._type == scalesWithNeighboringTiles)
+                {
+                    fittingNeighbors++;
+                }
+            }
+            if (fittingNeighbors < minNeighbors)
+            {
+                efficiency = 0.0f;
+            }
+            else if (fittingNeighbors >= maxNeighbors)
+            {
+                efficiency = 1.0f;
+            }
+            else {
+                efficiency = (float)fittingNeighbors / (float)maxNeighbors;
+            }
+        }
     }
 }
